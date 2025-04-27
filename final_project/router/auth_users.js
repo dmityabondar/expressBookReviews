cff50f7b5c4518517b8de8e111b2531e6010a224
Dmitrii Bondar <plasticbofficial@gmail.com>
2025-04-27 11:54:24 +0200

    Book Review App: Homework

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,58 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+  const isUserPresent = users.some(obj => Object.values(obj).includes(username));
+  return isUserPresent;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+  const user = users.find(u => u.username === username && u.password === password);
+  return !!user;
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please provide both username and password" });
+  }
+
+  const user = users.find(u => u.username === username);
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid credentials"
+    });
+  }
+
+  if (user.password !== password) {
+    return res.status(401).json({
+      message: "Invalid credentials."
+    });
+  }
+
+  const accessToken = jwt.sign({ username: user.username }, 'your_secret_key');
+  req.session.accessToken = accessToken;
+
+  return res.json({
+    message: "Login successful",
+    accessToken
+  });
 });
 
-// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.session.username;
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  if (!review) return res.status(400).json({ message: "Please provide a review" });
+  if (!books[isbn]) return res.status(404).json({ message: "Book not found" });
+  if (!books[isbn].reviews) books[isbn].reviews = {};
+  if (books[isbn].reviews[username]) {
+    books[isbn].reviews[username] = review;
+    return res.json({ message: "Review modified successfully" });
+  }
+  books[isbn].reviews[username] = review;
+  return res.json({ message: "Review added successfully" });
 });
 
 module.exports.authenticated = regd_users;
